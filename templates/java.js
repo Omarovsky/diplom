3b9ce222d2a74f42fde85eb29c41804e57f9de6f
Wetrum <74416823+Omarovsky@users.noreply.github.com>
2025-06-18 16:46:31 +0500

    Delete templates/java.js

--- templates/java.js
+++
@@ -1,7 +0,0 @@
-function updateScale() {
-  const scaleX = window.innerWidth / 1920;
-  const scaleY = window.innerHeight / 1080;
-  const scale = Math.min(scaleX, scaleY);
-  document.documentElement.style.setProperty('--scale', scale);
-}
-
